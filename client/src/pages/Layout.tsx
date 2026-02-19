c85a7e0d0994ada690594a49ecf722b3caf4fb5f
pradyumn8 <96361733+pradyumn8@users.noreply.github.com>
2026-02-19 21:30:38 +0530

    Created dashboard & Sidebar

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom"
+import Sidebar from "../components/Sidebar"
+import BottomNav from "../components/BottomNav"
 
 const Layout = () => {
     return (
-        <>
-            Layout
-            <Outlet />
-        </>
+        <div className="layout-container">
+            <Sidebar />
+            <div className="flex-1 overflow-y-scroll">
+                <Outlet />
+            </div>
+            <BottomNav/>
+        </div>
     )
 }
 
